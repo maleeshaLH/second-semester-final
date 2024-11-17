34610ad6d9d83f8ce8f390fede7efd6a2c7c2d47
maleeshaLH <lhmv114@gmil.com>
2024-11-17 23:13:24 +0530

    Create slider bar

---
+++ Frontend/js/loader.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const loader = document.getElementById("page-loader");
+  const mainContent = document.getElementById("content"); // Updated to use "content"
+
+  setTimeout(() => {
+    if (loader) loader.style.display = "none";
+    if (mainContent) mainContent.style.display = "block";
+  }, 1000);
+});
